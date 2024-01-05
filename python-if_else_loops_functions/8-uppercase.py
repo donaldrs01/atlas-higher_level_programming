8989eb3740b46e60e9ad3733fede8eee56634f4c
Your Name <you@example.com>
2024-01-05 12:22:54 -0800

    fixed typo - function that re-writes string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     result = ""
     for char in str:
         result += chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print("{}".format(reuslt))
+        print("{}".format(result))
