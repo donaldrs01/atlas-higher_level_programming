baad59e9aeb76ac1cba1125956007caa342021f2
Your Name <you@example.com>
2024-01-05 12:21:33 -0800

    function to re-write string in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        result += chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print("{}".format(reuslt))
