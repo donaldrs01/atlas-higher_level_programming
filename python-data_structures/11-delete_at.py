66a9d0e21930ba45a92bdff2d50fd194574c1024
Your Name <you@example.com>
2024-01-08 12:46:26 -0800

    simplified with del

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,7 +4,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):  # checks on index range
         return my_list
 
-    new_list = [my_list[i] for i in range(len(my_list)) if i != idx]
-    # reprints list but excludes value of index input
-
-    return new_list
+    del my_list[idx]
+    return my_list
