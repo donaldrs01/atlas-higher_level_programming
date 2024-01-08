a7f2ebbc78d08395e4c7b79a55ce4c4803910450
Your Name <you@example.com>
2024-01-08 12:36:44 -0800

    copies list, replaces with true/false depending on divisibility

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list == []):
+
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:  # checks for divisibility
+            new_list[i] = True  # prints true in new list
+        else:
+            new_list[i] = False  # prints false if not % by 2
+    return new_list
