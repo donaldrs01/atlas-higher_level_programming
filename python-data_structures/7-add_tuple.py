c3f7a112b424b08047513da79115c841f9a39af9
Your Name <you@example.com>
2024-01-08 12:16:53 -0800

    changed return statement

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,4 +3,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)  # pads tuples w/ 0 if smaller than 2 elements
     tuple_b += (0, 0)
 
-    return (sum(tuple_a[:2]), sum(tuple_b[:2]))
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
