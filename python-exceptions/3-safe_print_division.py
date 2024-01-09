56429761f8e6bd7a155feef5b954b7258896699e
Your Name <you@example.com>
2024-01-09 13:29:28 -0800

    added flag for other errors and fixed typo

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,9 +2,12 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except ZeroDivisonError:
+    except ZeroDivisionError:
         print("Can't divide by 0, bozo.")
         result = None
+        return None
+    except Exception as error:  # flags other unexpected errors
+        print("Unexpected error: {}".format(error))
         return None
     finally:
         print("Inside result: {}".format(result))
