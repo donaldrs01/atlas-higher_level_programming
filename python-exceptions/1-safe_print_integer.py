26168cc9246ba7413dd320ed0317fe24db835e95
Your Name <you@example.com>
2024-01-09 14:10:35 -0800

    specified error types to return false

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except if (ValueError, TypeError):
+    except (TypeError, ValueError):  # if failure to run, return False
         return False
