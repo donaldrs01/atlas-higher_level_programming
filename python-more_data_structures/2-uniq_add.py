81e8d7cd19a37c3b2dd92d4fcab6a36f27f402fd
Your Name <you@example.com>
2024-01-09 06:02:42 -0800

    got rid of test case in file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,3 @@ def uniq_add(my_list=[]):
         if i not in new_list:  # only adds unique values to new list
             new_list.append(i)
     return sum(new_list)
-
-my_list = [1, 2, 2, 3, 4, 5, 5]
-result = uniq_add(my_list)
-print(result)
