85a06ba6950863b74e5eb58eb0d220c9b2f4249f
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-12 19:26:26 +0000

    Square class definition and proper documentation (I think?)

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""
+This is a basic creation/description of a 'Square' class created on Python
+"""
+
+
+class Square:
+
+    """
+    Square definition for the square class
+
+    """
