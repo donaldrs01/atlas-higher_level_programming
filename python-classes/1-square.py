04e38f7d33832837d6f7f0f091c2e1fc938365e1
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-12 21:30:51 +0000

    defines Square class with size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Defining a class 'Square'
+"""
+
+
+class Square:
+    """
+    'Square' class
+    """
+    def __init__(self, size):
+        """
+        Constructor method defines size of square
+        """
+        self.__size = size
+        # __ makes size private attribute
