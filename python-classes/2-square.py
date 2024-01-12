26f42fda924c731f3c9abc1984de8a3578da5338
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-12 22:00:01 +0000

    similar to last task but with added Errors for Type/Value

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Creation of square class with size attribute
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+
+    def __init__(self, size=0):
+        """
+        Method for square class that defines size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
