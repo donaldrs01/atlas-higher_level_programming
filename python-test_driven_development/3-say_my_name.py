e7185d660b84dbac9750a06f874ebe9459ac14e8
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 22:09:02 +0000

    function that prints first/last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+"""
+Module that defines function that prints first and last name based on input
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    Function that prints 'My name is <first name> <last name>'.
+
+    Args:
+        first_name (str) : The first name to be printed
+        last_name (str) : The last name to be printed
+
+    Errors:
+        TypeError: If <first_name> or <last_name> are not proper strings
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
