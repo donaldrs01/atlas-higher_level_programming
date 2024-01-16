db73e2ad9caaa83dc02e7edfd0e86441ed630279
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 22:25:28 +0000

    beginning of indent function

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""
+Module for function that searches for characters and prints new lines
+"""
+
+
 def text_indentation(text):
+    """
+    Function that searches for special characters and follows them with 2 new lines
+
+    Args:
+        text (str) : the inputted text
+
+    Errors:
+        TypeError : if text is not a proper string
+    """
+
+    if type(text) is not string:
+        raise TypeError("text must be a string")
+
