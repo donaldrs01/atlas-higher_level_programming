c621a58d780bc3e1858805d37ea355d94da57afa
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 14:20:30 +0000

    re-did documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -2,7 +2,7 @@
 """
 This module will define a new class Rectangle
 """
-class Rectangle
-"""
-Definition of the rectangle class
-"""
+class Rectangle:
+    """
+    Definition of the rectangle class
+    """
