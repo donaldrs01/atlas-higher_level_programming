b631c50f27705d3a24b2e8dcfab3d5be46204d24
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 15:06:28 +0000

    fixed indentation issues - is documentation better? we shall see

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -34,12 +34,12 @@ class Rectangle:
         Args:
             value (int) : the value of new rectangle's width
         """
-    if type(value) is not int:
-        raise TypeError("width must be an integer")
-    elif value < 0:
-        raise ValueError("width must be >= 0")
-    else:
-        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
