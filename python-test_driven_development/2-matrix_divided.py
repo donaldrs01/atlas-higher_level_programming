2f96fb303211bb730f3a861b7aa21092f4518cd9
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 22:02:49 +0000

    completed division function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,3 +27,7 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+
+    return new_matrix
