4d4d63e364f02c52857466aa62981978de67ffaf
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 15:02:41 +0000

    fixed indentation issues - is documentation better? we shall see

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -20,46 +20,45 @@ class Rectangle:
         self.height = height
 
     @property
-        def width(self):
-            """
-            Getter for width
-            """
-            return self.__width
+    def width(self):
+        """
+        Getter for width
+        """
+        return self.__width
 
     @width.setter
-        def width(self, value):
-            """
-            Setter for width attribute of rectangle
+    def width(self, value):
+        """
+        Setter for width attribute of rectangle
 
-            Args:
-                value (int) : the value of new rectangle's width
-            """
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+        Args:
+            value (int) : the value of new rectangle's width
+        """
+    if type(value) is not int:
+        raise TypeError("width must be an integer")
+    elif value < 0:
+        raise ValueError("width must be >= 0")
+    else:
+        self.__width = value
 
     @property
-        def height(self):
-            """
-            Getter for height attribute
-            """
-            return self.__height
+    def height(self):
+        """
+        Getter for height attribute
+        """
+        return self.__height
 
     @height.setter
-        def height(self, value):
-            """
-            Setter for height
+    def height(self, value):
+        """
+        Setter for height
 
-            Args:
-                value (int) : the value of the new rectangle's height
-            """
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = value
-
+        Args:
+            value (int) : the value of the new rectangle's height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
