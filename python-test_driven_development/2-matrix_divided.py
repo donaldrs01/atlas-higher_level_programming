a06f9b45ab4b1d34ba658f7acc8e03e4401f8ef1
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-16 21:50:53 +0000

    covering all error types

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+"""
+Module that divides all elements in a matrix
+"""
+
+
 def matrix_divided(matrix, div):
+    """
+    Function that divides all elemeents in a matrix by a specific number
+    
+    Args:
+        matrix (set) : a set of numbers
+        div (int) : the divisor
+
+    Errors:
+        TypeError: If 'div' is a non-number
+        TypeError: If each row of matrix is not the same size
+        TypeError: If 'matrix' is not fully made up of integers or floats
+        ZeroDivisionError: If 'div' is equal to 0
+    """
+
