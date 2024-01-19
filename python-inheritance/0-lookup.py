6f2a059c83d5391eb724ffcad595ce5e43cc46cc
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-19 21:24:25 +0000

    uses built-in dir() function to provide list of all methods of a given object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module allowing us to lookup a list of all attributes and methods of object passed through function
+"""
+
+
+def lookup(obj):
+    """
+    Takes object as argument and returns a list of all attributes and methods associated with that object
+
+    Args:
+        obj : the object whose methods will be retrieved
+    """
+
+    return list(dir(obj)) 
---
+++ python-inheritance/README.md
@@ -0,0 +1,1 @@
+This README is for my project work dealing with inheritance in OOP
