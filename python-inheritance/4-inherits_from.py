09b5592f77429328a9af282e5f74bd430bfc91b1
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 14:56:28 +0000

    issubclass built-in checks if obj is subclass of specified class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for inherits_from function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function evaluates whether obj is instance of class
+    that inherited from specified class
+
+    Args:
+        obj : the object to evaluate
+        class : the specified class
+
+    Returns:
+        True if obj is instance of class inherited from a_class
+        False otherwise
+    """
+    return issubclass(type(obj), a_class)
+    # type(obj) gets type of object and then checks
+    # whether that type is subclass of 'a_class'
