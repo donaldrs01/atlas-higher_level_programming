d3e963af9981cd6ab7532ba62a1f990b04f318fb
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 16:54:43 +0000

    integer validation checks

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -29,5 +29,5 @@ class BaseGeometry:
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value is <= 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
