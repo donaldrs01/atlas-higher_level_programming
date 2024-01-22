b6c413c8098a382c38737a7c2fc527d2d2144ab9
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 21:05:43 +0000

    read and  prints out filename input

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for reading a file and printing content to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads file and prints it out to stdout
+
+    Args: 
+        filename (str) : the name of the text file
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        read_data = file.read()
+        print(read_data)
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+README file for my directory working with input/output in Python
