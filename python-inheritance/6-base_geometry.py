2c550e4548fc02f65061b3d6b1762997cc8c1f0f
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 15:17:18 +0000

    add unfinished area method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    Class declaration for BaseGeometry
+    """
+    def area(self):
+        """
+        Basic and incomplete method for area calculation
+        """
+        raise Exception ("area() is not implemented")
