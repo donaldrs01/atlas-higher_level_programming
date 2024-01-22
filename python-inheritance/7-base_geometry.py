deac8af87c05aee72d0bcfa0a1211b8e21c9b0d4
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 16:51:49 +0000

    integer validation checks

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,3 +2,32 @@
 """
 Module for BaseGeometry class
 """
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        Incomplete method that raises Exception
+        """
+        raise Exception "area() is not implemented"
+
+    def integer_validator(self, name, value):
+        """
+        Validates the passed value
+
+        Args:
+            name : the name of the value (str)
+            value : the value (int)
+
+        Raises:
+            TypeError : if value is not integer
+            ValueError : if value is less than or equal to 0
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value is not >= 0:
+            raise ValueError("{} must be greater than 0".format(name))
