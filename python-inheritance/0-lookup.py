f428f6ab5bb41f7db21fb21df6e6c2984f281d1b
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 20:23:00 +0000

    shortened lines

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Module allowing us to lookup a list of all attributes and methods of object passed through function
+Module allowing us to lookup a list of all attributes
+and methods of object passed through function
 """
 
 
 def lookup(obj):
     """
-    Takes object as argument and returns a list of all attributes and methods associated with that object
+    Takes object as argument and returns a list of all
+    attributes and methods associated with that object
 
     Args:
         obj: the object whose methods will be retrieved
