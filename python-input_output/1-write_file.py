5cb3e2461ba702115eb2b09b7f739d913a51bcaf
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 21:20:44 +0000

    stores written characters and returns

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that contains function that writes string
+to text file and returns # of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes inputted text into given file
+
+    Arguments:
+        filename (str) : name of file being added to
+        text (str) : text to be written into file
+
+    Return:
+        The number of characters written
+    """
+
+    with open(filename, mode="w", enccoding="utf-8") as file:
+        chars_written = file.write(text)
+
+    return chars_written
