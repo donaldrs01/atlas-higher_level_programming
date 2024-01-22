94d5366b02692454163a0feeed4636a05c573a56
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-22 14:49:39 +0000

    module and function documentation

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module evaluates for whether object is instance or sub-instance
+of specified class
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that checks if object is instance of / inherited from
+    the specified class
+
+    Args:
+        obj : object to evaluate
+        a_class: specified class
+
+    Returns:
+        True is obj is instance of a_class or its subclass
+        False otherwise
+    """
