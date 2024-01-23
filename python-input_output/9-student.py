e62ad92c8428b4fb8c2980ace109e6e0b58b16f9
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-23 18:26:02 +0000

    Student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module for Student class definition
+"""
+
+
+class Student:
+    """
+    Student class
+
+    Attributes:
+        first_name
+        last_name
+        age
+    
+    Methods:
+        def to_json(self)  :
+            returns dictionary representation of instance of Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes new instance of Student
+
+        Args:
+            first_name (str) : first name of student
+            last_name (str) : last name of student
+            age (int) : age of student
+        """
+        self.first_name = first_name  # initializes attributes based on input
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """
+        Provides dict representation of instance of Student
+
+        Returns:
+            dict: Dictionary with keys as attribute names and values
+            as attribute values
+        """
+        return self.__dict__  # returns attributes of object
