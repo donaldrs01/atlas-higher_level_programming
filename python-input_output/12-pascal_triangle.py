c441ff8834819c88b8416f51a6a6669887bd2c19
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-24 01:15:19 +0000

    pascals triangle logic

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,4 +8,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     else:
+        triangle = []
+        for i in range(n):  # iterate through each row of triangle
+            row = [1] * (i + 1)  # first element always 1 : prev_row + 1
+            for j in range(1, i):  # iterates through each element in new row
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                #  [i - 1] = previous row in triangle
+                #  [j - 1] = previous element on row
+                #  gets sum of two elements directly above current
+        triangle.append(row)
 
+        return triangle
