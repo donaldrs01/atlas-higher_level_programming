c5f1b199ded6758ce002cdb92681051bf795d4a6
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-25 18:29:00 +0000

    changed <= to just <

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
@@ -117,7 +117,7 @@ class Rectangle(Base):
         """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
