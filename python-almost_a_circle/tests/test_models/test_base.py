0e0f6db2f64067b3727e95d199805eaa65f38b64
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-25 15:30:23 +0000

    test files setup

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,3 +2,8 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
+    def test_base_instance_creation(self):
+        obj1 = Base()  # Output : 1
+        obj2 = Base()  # Output : 2
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
