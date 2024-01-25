8d7842443a81d79c36d224ac54ad8d8fbf091e4a
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-25 16:07:36 +0000

    second base test for sync between ID and nb_objects count

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,8 +7,20 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
+
     def test_base_instance_creation(self):
+        """
+        Tests Base() instantiation and unique IDs
+        """
         obj1 = Base()  # Output : 1
         obj2 = Base()  # Output : 2
         self.assertEqual(obj1.id, 1)
         self.assertEqual(obj2.id, 2)
+
+    def test_id_sync(self):
+        """
+        Tests sync between nb_objects and ID #
+        """
+        obj1 = Base()
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), obj1.id)
+
