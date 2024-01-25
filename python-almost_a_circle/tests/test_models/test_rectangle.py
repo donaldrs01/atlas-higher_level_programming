1aec4b355c0b79bcdf2255576713213ea32ec922
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-25 18:58:59 +0000

    added setUp and tearDown to reset test cases

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,43 @@
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    Tests Rectangle class
+    """
+
+    def setUp(self):
+        """
+        Imports base model and instantiates Rectangle class
+        """
+        Base._Basei__nb_objects = 0
+
+    def tearDown(self):
+        """
+        Resets to setUp conditions
+        """
+        pass
+
+    def test_class(self):
+        """
+        Tests Rectangle class type
+        """
+        self.assertIsInstance(Rectangle(), Rectangle)  # instance is type Rectangle
+
+    def test_inheritance(self):
+        """
+        Tests if Rectangle is sub-class of Base
+        """
+        self.assertTrue(issubclass(Rectangle, Base))
+    
+    def test_constructor_no_args(self):
+        """
+        Tests constructor when no arguments passed
+        """
+        with self.assertRaises(TypeError) as e:
+            r = Rectangle()
+        s = "Rectangle.__init__() missing 2 required positional arguments: 'width' and 'height'"
+        self.assertEqual(str(e.exception), s)
+
