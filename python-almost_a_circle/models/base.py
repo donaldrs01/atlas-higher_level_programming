957d11b0447270e4e64e649a61f837a16c421175
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-25 16:09:12 +0000

    clarified

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,5 +26,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            Base.__nb_objects += 1  # increment # of instances count
+            self.id = Base.__nb_objects  # assign count to ID #
