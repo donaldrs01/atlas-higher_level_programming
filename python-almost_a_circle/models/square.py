cf940f7ae3398b6f128fb566072804de65bffa5c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-01-26 17:16:48 +0000

    clarifying comments

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,12 +13,15 @@ class Square(Rectangle):
         size (int) : the size of one side of square
         x (int) : x coordinate
         y (int) : y coordinate
+        ID (int) : ID # assigned to instance
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Constructor
+        Constructor that initializes attributes of Square
         """
         super().__init__(size, size, x, y, id)
+        #  calls Rectangle constructor and passes through values
+        #  'size' value is passed as both width and height
 
     def __str__(self):
         """
