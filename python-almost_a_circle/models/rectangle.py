a2e44835e1979f88ea6d4633218bb03e32300280
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-01-26 16:16:22 +0000

    added update to assign attributes to specific argument order

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -145,6 +145,26 @@ class Rectangle(Base):
                 print(' ' * self.__x + "#" * self.__width)
                 # add spaces before printing row based on x input
 
+    def update(self, *args):
+        """
+        Assigns attributes to specific argument order:
+            [0] - ID attribute
+            [1] - Width attribute
+            [2] - Height attribute
+            [3] - x attribute
+            [4] - y attribute
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def __str__(self):
         """
         Returns Rectangle description in string form
