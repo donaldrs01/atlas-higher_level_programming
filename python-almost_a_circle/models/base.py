186a4df5a108f42b20bc19286e69c250d2a72090
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-01-29 16:38:47 +0000

    JSON convert dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """
 Module for Base class definition
 """
-
+import json
 
 class Base:
     """
@@ -27,3 +27,21 @@ class Base:
         else:
             Base.__nb_objects += 1  # increment # of instances count
             self.id = Base.__nb_objects  # assign count to ID #
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Static method called before instantiation that
+        returns JSON representation of key:value pairs
+        of all Base instances
+
+        Args:
+            list_dictionaries : list of dictionaries of class Base
+
+        Return:
+        JSON string representation of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+           return json.dumps(list_dictionaries)
