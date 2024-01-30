2f13b1800cf00ba804033b5e621b761f4463cabd
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-01-30 18:41:02 +0000

    more square tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -19,4 +19,25 @@ class TestSquare(unittest.TestCase):
         Tests Square class type
         """
         s = Square(6)
-        self.assertIsInstance(s, Square, "s is not an instance of Square")
+        self.assertIsInstance(s, Square)
+
+    def test_construction_size_arg(self):
+        """
+        Tests constructor when size arg is passed
+        """
+        s = Square(6)
+        self.assertEqual(s.size, 6)
+        self.assertEqual(s.x, 0)
+        self.assertEqual(s.y, 0)
+        
+    def test_construction_all_args(self):
+        """
+        Tests constructor when all args passed
+        """
+        s2 = Square(5, 4, 4, 15)
+        self.assertEqual(s2.size, 5)
+        self.assertEqual(s2.x, 4)
+        self.assertEqual(s2.y, 4)
+        self.assertEqual(s2.id, 15)
+
+
