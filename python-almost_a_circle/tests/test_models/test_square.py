95facdd029fbf8ac7a071da0f24dbde46c61546f
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-01-30 19:47:57 +0000

    ValueError, not TypeError for square validation

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -40,4 +40,47 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s2.y, 4)
         self.assertEqual(s2.id, 15)
 
+    def test_negative_size(self):
+        """
+        Tests constructor when receives negative value
+        """
+        with self.assertRaises(ValueError):
+            s = Square(-5)
+
+    def test_size_zero(self):
+        """
+        Tests constructor when it receives 0 as size value
+        """
+        with self.assertRaises(ValueError):
+            s = Square(0)
+
+    def test_str_input(self):
+        """
+        Tests constructor when it receives a string as a size value
+        """
+        with self.assertRaises(TypeError):
+            s = Square("test")
+
+    def test_neg_x_value(self):
+        """
+        Tests constructor when 'x' value is negative
+        """
+        with self.assertRaises(ValueError):
+            s = Square(10, -4)
+
+    def test_neg_y_value(self):
+        """
+        Tests constructor when 'y' value is negative
+        """
+        with self.assertRaises(ValueError):
+            s = Square(10, 4, -4)
+
+    def test_square_to_dict(self):
+        """
+        Tests that to_dictionary method properly returns dict definition
+        """
+        s = Square(4, 1, 1, 15)
+        dict_def = s.to_dictionary()
+        expected_def = {"id": 15, "size": 4, "x": 1, "y": 1}
+        self.assertEqual(expected_def, dict_def)
 
