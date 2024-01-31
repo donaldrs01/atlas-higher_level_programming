a9e80afd9c3561dcda8394a1cee2f23730e2391c
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-01-31 20:00:41 +0000

    test

---
+++ python-almost_a_circle/models/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def calculator():
+    x = int(input("What is your x value?"))
+    y = int(input("What is your y value?"))
+    result = x + y
+    print(f"The sum of x and y is {result}")
+
+calculator()
