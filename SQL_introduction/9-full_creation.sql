4987d614b5d2857744b49ea4a26c44eea1949a8c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-15 15:34:51 -0600

    creates new table with values added into mulitple rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
 -- Creates second_table in hbtn_0c_0 database and adds multiple rows
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
-    name VARCHAR (256),
+    name VARCHAR(256),
     score INT
 );
 
-INSERT INTO hbtn_0c_0.second_table(id, name, score) VALUES
+-- Inserts rows with specified values into second_table
+INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
     (1, 'John', 10),
     (2, 'Alex', 3),
     (3, 'Bob', 14),
