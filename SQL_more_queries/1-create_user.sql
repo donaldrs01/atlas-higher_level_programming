6d93dceea17724300ddead9b6a4f857d73ca938b
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-16 11:11:34 -0600

    create new user with specified password with all privileges for tables/databases

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates a user user_0d_1 on server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- *.* gives privileges for all databases and tables
