af206e100313c04e5ed3441bbdf7eacf56af2b21
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-18 08:18:27 -0600

    pull id and name from cities table, not state_id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Using subquery to select all cities in California
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
-SELECT * FROM cities WHERE state_id = @california_id
+SELECT id, name FROM cities WHERE state_id = @california_id
 ORDER BY id ASC;
