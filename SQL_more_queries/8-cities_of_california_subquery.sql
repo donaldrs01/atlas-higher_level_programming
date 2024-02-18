a8c5145fa053a688096cfa33b1c745f2e6af5edc
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-18 08:17:02 -0600

    remove statement using hbtn_0d_usa database?

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- Using subquery to select all cities in California
-USE hbtn_0d_usa; 
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
 SELECT * FROM cities WHERE state_id = @california_id
 ORDER BY id ASC;
