91b35b76c141988d7c36269b63a196f528bb1d88
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-19 13:59:03 -0600

    left join to list all shows in database (NULL if no genre)

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in _tvshows database, regardless of whether genre is listed or not
+-- Will display 'NULL' if no genre for show
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- Includes all records from left table (tv_shows) regardless of whether there is a match in right table (tv_show_genres)
+-- Links 'id' column from tv_shows with 'show_id' column from tv_show_genres
+ORDER BY tv_shows.title ASC, genre_id ASC;
