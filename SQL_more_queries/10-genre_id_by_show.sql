b4764f1ea4502fb642335750caa086ff88252c36
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-19 13:48:40 -0600

    typo fix

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- List all shows contained in hbtn_0d_tvshows database that have at least one genre linked
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
