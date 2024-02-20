b0ed7157a0af267f12deef923e94768bc5e69713
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 08:17:46 -0600

    pycode fixes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,7 @@
 import MySQLdb
 import sys
 
+
 def list_all_states(user, password, db_name):
     """Function that will connect to hbtn_0e_0_usa database and list all states
 
@@ -17,15 +18,15 @@ def list_all_states(user, password, db_name):
                          user=user,
                          password=password,
                          database=db_name)
-    cursor = db.cursor()  # creates cursor object 
+    cursor = db.cursor()  # creates cursor object
     cursor.execute("SELECT * FROM states ORDER BY id ASC;")
     allstates = cursor.fetchall()
     for state in allstates:
         print(state)
-    
+
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
-
