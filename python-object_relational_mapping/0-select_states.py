868da4131aeb76433242ab069eaf43479407b2c2
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 07:41:44 -0600

    first attempt at 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+    """Lists all states in 'hbtb_0d_usa' database using MySQLdb module
+    """
+import MySQLdb
+import sys
+
+def list_all_states(user, password, db_name):
+    """Function that will connect to hbtn_0e_0_usa database and list all states
+
+    Args:
+        user (str): MYSQL username
+        password (str): MYSQL password
+        db_name (str): name of database
+    """
+    db = MYSQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user.
+                         password=password.
+                         database=db_name)
+    cursor = db.cursor()  # creates cursor object 
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    allstates = cursor.fetchall()
+    for state in allstates:
+        print(state)
+    
+    cursor.close()
+    db.close()
+
