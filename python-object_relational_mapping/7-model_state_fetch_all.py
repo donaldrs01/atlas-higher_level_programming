1ba1bf7fdf745557073fda65353aeab1a2ba2155
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 17:35:58 -0600

    module documentation

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Lists all 'State' objects in hbtn_0e_6_usa database
+"""
+Lists all 'State' objects in hbtn_0e_6_usa database
 """
 from model_state import Base, State
 from sqlalchemy import create_engine
