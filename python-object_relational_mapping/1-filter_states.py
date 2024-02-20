907ee4711a3cf8acc415f31c7f546840ea0e81d7
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 10:55:37 -0600

    filters states based on first letter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,3 +4,30 @@
 import MySQLdb
 import sys
 
+def list_N_states():
+    """Connects with database and lists states with names starting with 'N'
+    """
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host = "localhost",
+        port = 3306,
+        user = user,
+        password = password,
+        database = database_name
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'; ")
+    #  searches for state names that start with 'N'
+    Nstates = cursor.fetchall()
+    for state in Nstates:
+        print(state)
+    
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_N_states()
