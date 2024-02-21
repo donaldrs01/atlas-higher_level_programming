db6fe5d859622ede2fd73beca31f5e28256b88cd
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 19:51:53 -0600

    add new state 'Louisiana' to states database through SQLAlchemy

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Adds the State object 'Louisiana' to hbtn_0e_6_usa database
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db_name}'
+    )
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #  create new State object for Louisiana
+    new_state = State(name='Louisiana')
+    #  add State object to session
+    session.add(new_state)
+    #  commit to database
+    session.commit()
+    #  print state.id
+    print(new_state.id)
+
+    session.close()
