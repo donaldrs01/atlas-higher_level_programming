0f227ec5a15cc14795deb6d1157c3953e10adb0b
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 18:15:19 -0600

    prints first state listed in table - otherwise prints nothing

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints the first 'State'
-object from hbtn_0e_6_usa database
+Script that prints the first 'State' object from hbtn_0e_6_usa database
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
