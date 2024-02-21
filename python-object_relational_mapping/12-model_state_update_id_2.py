155b79c5aa4bb4400b9696f1b0841a485a5f5a6c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-02-20 20:09:54 -0600

    updates name value at id=2 to New Mexico

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Updates 'states' table to change name of State object where id=2
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+    )
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    #  query states table and searches for first result where state.id = 2
+    update_state = session.query(State).filter(State.id == 2).first()
+
+    update_state.name = 'New Mexico'
+
+    session.commit()
+
+    session.close()
