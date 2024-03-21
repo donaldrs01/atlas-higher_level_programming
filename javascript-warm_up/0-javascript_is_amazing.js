81e93c2a3afa673b35d0d6c069367dd26971dc6e
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-21 12:07:01 -0500

    strings must use single quote

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Declare variable and assign string value
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 // Print to console
 console.log(myVar);
