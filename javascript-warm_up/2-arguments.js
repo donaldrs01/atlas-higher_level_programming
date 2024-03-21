048ad1ac03234b6b4d2ff60d2843aad694967105
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-21 12:26:20 -0500

    using process.argv to print statements depending on number of passed args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,13 +4,13 @@
 Check for number of arguments
 Subtract 2 because argv[0] is path to node.js and argv[1] is path to script
 */
-const number_of_args = process.argv.length - 2;
+const numberOfArgs = process.argv.length - 2;
 
 // Print statement based on number of arugments
-if (number_of_args === 0) {
-    console.log('No argument');
-} else if (number_of_args === 1) {
-    console.log('Argument found');
+if (numberOfArgs === 0) {
+  console.log('No argument');
+} else if (numberOfArgs === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
