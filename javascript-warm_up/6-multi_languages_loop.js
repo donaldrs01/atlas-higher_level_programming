890ca371544be7c18b4ebff9ad6094b12e526743
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-21 14:09:01 -0500

    for loop to iterate through 3 print statements

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const printStatements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// For loop to iterate through eaech printStatement
+for (let i = 0; i < printStatements.length; i++) {
+  console.log(printStatements[i]);
+}
