3cc99253ad6b9c58767b73d83e87a1b3a07adbef
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-22 09:29:39 -0500

    nested for loop to print square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const size = parseInt(process.argv[2]);
 if (Number.isInteger(size)) {
   for (let i = 0; i < size; i++) {
     // Iterate over each row in square
-    let row = '';
+      let row = '';
     for (let j = 0; j < size; j++) {
       // append 'X' to each 'row' variable inside each column
       row += 'X';
