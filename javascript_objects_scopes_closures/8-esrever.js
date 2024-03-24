572693415c6882c03f1f6878c655f19bc9e6ed02
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-24 12:56:32 -0500

    reverse-iterate through list, push() elements to new array

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const reversedList = [];
+  // Start at last element, proceed backwards through list
+  for (let i = list.length - 1; i > 0; i--) {
+    // Use push() method to add items to new array
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
