759f9e16e581a94156562e5cfd99924bf47d04ab
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-24 12:57:27 -0500

    i >= 0 in order to include index 0

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const reversedList = [];
   // Start at last element, proceed backwards through list
-  for (let i = list.length - 1; i > 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     // Use push() method to add items to new array
     reversedList.push(list[i]);
   }
