0a7156c617c70242463d3b9aa592063df8df534d
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-24 13:15:37 -0500

    initialize count outside of function so it doesn't reset

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-exports.logMe = function (tem) {
-    
+// Initialize outside of function so it doesn't reset each time
+let indexCount = 0;
+exports.logMe = function (item) {
+    // Log index value and print 'item' passed through
+    console.log(indexCount + ": " + item)
+    // Increment count to account for next item
+    indexCount++;
 }
