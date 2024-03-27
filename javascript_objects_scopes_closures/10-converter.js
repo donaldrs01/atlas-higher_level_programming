51650d26683e8ac7ffb0e5817813e7ac7a947a59
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-26 22:08:29 -0500

    task 10

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return function (number) {
+    // convert number to string using toString() in the specified base
+    return number.toString(base);
+  };
+};
