a98570e3c0e058f4385fb861c7647eecb5ca3ccc
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-28 10:07:33 -0500

    request to urlPath, response.statusCode to print status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const request = require('request');;
+const request = require('request');
 const urlPath = process.argv[2];
 
 request(urlPath, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log(`code: ${response.statusCode}`);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
 });
