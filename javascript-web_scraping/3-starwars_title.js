554da60b6eb69c4885d6c5077f560c4340f8268e
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-03-28 10:18:30 -0500

    construct URL, parse JSON response, access title attribute

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,12 +4,12 @@ const movieID = process.argv[2];
 const urlPath = `https://swapi-api.hbtn.io/api/films/${movieID}/`;
 
 request(urlPath, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    // Convert JSON response from API into JS-compatible object
-    const film = JSON.parse(body);
-    // Use dot notation to access object attributes (film title)
-    console.log(film.title);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Convert JSON response from API into JS-compatible object
+  const film = JSON.parse(body);
+  // Use dot notation to access object attributes (film title)
+  console.log(film.title);
 });
