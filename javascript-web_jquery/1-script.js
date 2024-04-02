5a284d8c68d3c0c00c9856777b21eb507e528f1a
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-04-02 12:43:56 -0500

    using jQuery to target header and change text color to red

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+/* global $ */
+// Wait for page DOM to load
+$(document).ready(function () {
+  // Once loaded, target the header and change color to red
+  $('header').css('color', '#FF0000');
+});
