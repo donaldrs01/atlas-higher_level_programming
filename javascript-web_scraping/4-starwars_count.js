dff919378de830cce9802c0be2be87744288ff7e
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-04-02 11:11:34 -0500

    wedgeFilms count

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,18 @@ const apiURL = process.argv[2];
 const charID = 18;
 
 request(apiURL, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    /* Prase JSON resposne, search for films with specific charID and assign var,
+  if (error) {
+    console.error(error);
+    return;
+  }
+  /* Prase JSON resposne, search for films with specific charID and assign var,
     print length of var */
-})
+  // Specifically parse the 'results' listing on the page
+  const films = JSON.parse(body).results;
+  // Use filter() to filter film array by searching for 'characters' that incluke charID 18
+  const wedgeFilms = films.filter(film => {
+    return film.characters.some(character => character.includes(`/api/people/${charID}/`));
+  });
+
+  console.log(wedgeFilms);
+});
