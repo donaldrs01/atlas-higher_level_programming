3bbe5576502b0dd63bc4dea085813078016487bd
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-04-02 15:20:17 -0500

    toggleClass to swap between header font colors

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+/* global $ */
+// Wait for page DOM to load
+$(document).ready(function () {
+    // Click event that triggers toggle function
+    $('#toggle_header').click(function() {
+        // On click, use toggleClass to switch between font colors for <header>
+        $('header').toggleClass('red green');
+    });
+});
